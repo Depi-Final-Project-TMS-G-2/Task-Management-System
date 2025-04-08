d8b3c8cf3be4c7b50d2619103bff47b137f448a6
NourMahmoud992 <165487810+NourMahmoud992@users.noreply.github.com>
2025-04-08 11:58:39 +0200

    Add files via upload

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require('./images/trello.png'),
+    appIcon: require('./images/trello_name.png')
+  },
+  colors: {
+    secondary: '#0079bf'
+  }
+}
+
+export default assets
